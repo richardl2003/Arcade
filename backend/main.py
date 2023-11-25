7e516117f75591071c9d66517ce100a58524e905
shaansuthar <shaan.suthar@icloud.com>
2023-11-25 13:59:49 -0500

    Test subprocesses

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+import subprocess
 
 app = Flask(__name__)
 
@@ -12,6 +13,15 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        
+        # Run test.py script
+       
+        script_path = './test.py' # Using the relative path
+
+        try:
+            subprocess.run(['python', script_path], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred: {e}")
                
 @app.route('/video_feed')
 def video_feed():
