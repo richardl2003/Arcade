6d4390c834b43ae319bb51c3ced23be316d0e31b
Aiden Robinson <106715980+Aiden-Robinson@users.noreply.github.com>
2023-11-24 23:14:54 -0500

    video output

--- AidenTest.py
+++ AidenTest.py
@@ -1,2 +1,16 @@
+import cv2
+#print("hello world")
 
-print("hello world")
+cap= cv2.VideoCapture(0)
+
+while True:
+    ret, frame= cap.read()
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1)==ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
