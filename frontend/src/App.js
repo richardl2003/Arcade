c2c254efb7b98091c9f0b33d1814a4dc3255d5fa
richardl2003 <richardliy03@gmail.com>
2023-11-25 02:19:25 -0500

    WIP: integrating flask webcam and react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="game">
+          <iframe className="App-game" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" scrolling="none" frameborder="0"></iframe>
+        </div>
+        <div className="App-video">
+          <h3>Video Stream</h3>
+          <img src={'/webcam'} alt="video stream" />
+        </div>        
+      </div>
     </div>
   );
 }
