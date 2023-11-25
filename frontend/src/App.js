482956bd5b43d593a74c14dcf116f2ecfb29b58a
Luai Bashar <luai.hawa@gmail.com>
2023-11-25 10:44:56 -0500

    removed title warning for iframe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="App-header">
         <div className="game">
-          <iframe className="App-game" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" scrolling="none" frameborder="0"></iframe>
+          <iframe title="Camera" className="App-game" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" scrolling="none" frameborder="0"></iframe>
         </div>
         <div className="App-video">
           <h3>Video Stream</h3>
