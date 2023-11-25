9de968a698c6158c5a7d0b1d88a242abaaa7e2a6
richardl2003 <richardliy03@gmail.com>
2023-11-25 10:48:59 -0500

    feat: camera loads

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
         </div>
         <div className="App-video">
           <h3>Video Stream</h3>
-          <img src={'/webcam'} alt="video stream" />
+          <img src="http://localhost:5050/video_feed" alt="video stream" />
         </div>        
       </div>
     </div>
