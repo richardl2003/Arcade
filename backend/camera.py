4c028929abdfb613fb5dbcd9fe96c43229eef82b
richardl2003 <richardliy03@gmail.com>
2023-11-25 16:55:54 -0500

    WIP

--- backend/camera.py
+++ backend/camera.py
@@ -1,7 +1,13 @@
 import cv2
 
 class VideoCamera(object):
-    def __init__(self):
+    def __init__(self, input, flip, draw_landmarks, display, map, split):
+      self.input = input
+      self.flip = flip
+      self.draw_landmarks = draw_landmarks
+      self.display = display
+      self.map = map
+      self.split = split
       self.video = cv2.VideoCapture(0)
 
     def __del__(self):
@@ -10,4 +16,4 @@ class VideoCamera(object):
     def get_frame(self):
       ret, frame = self.video.read()
       ret, jpeg = cv2.imencode('.jpg', frame)
-      return jpeg.tobytes()
+      return jpeg.tobytes()  
