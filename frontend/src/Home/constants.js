43dc9236f4cbbcf507b45253adfb229a1b311c16
Luai Bashar <luai.hawa@gmail.com>
2023-11-25 13:41:28 -0500

    Added homepage assets and started navbar

--- frontend/src/Home/constants.js
+++ frontend/src/Home/constants.js
@@ -4,12 +4,12 @@ import Mario from "../Game/Mario/mario"
 export const routes = [
     {
       id: 1,
-      path: '/game/1',
+      path: '/templeRun',
       element: <TempleRun/>,
     },
     {
       id: 2,
-      path: '/game/2',
+      path: '/mario',
       element: <Mario/>,
     },
   ]
--- frontend/src/Home/homepage.js
+++ frontend/src/Home/homepage.js
@@ -1,20 +1,33 @@
 import './homepage.css';
 import { useNavigate } from 'react-router-dom';
+import Settings from '../Assets/Home/Settings.svg';
+import TempleRunBackground from '../Assets/Game/TempleRun/TempleRunBackground.png'
 
 function Homepage() {
 
   let navigate = useNavigate(); 
 
   return (
-    <div>
-        <button type='button' onClick={() => navigate('/game/1')}>
-          Temple Run
-        </button>
-        <button type='button' onClick={() => navigate('/game/2')}>
-          Mario
-        </button>
+    <div className='homepage'>
+      <img className='homeBackground' src={TempleRunBackground} />
+      <div className='navbar'>
+        <img className='settingsIcon' src={Settings}/>
+        <div className='time'>
+          1:30 PM
+        </div>
+      </div>
+      <div className='carousel'>
+        Hello
+      </div>
     </div>
   );
 }
+
+{/* <button type='button' onClick={() => navigate('/game/1')}>
+  Temple Run
+</button>
+<button type='button' onClick={() => navigate('/game/2')}>
+  Mario
+</button> */}
 
 export default Homepage;
