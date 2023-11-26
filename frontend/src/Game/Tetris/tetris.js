432e2308852b7c487416fc1db2dab9cefa0d85af
Aiden Robinson <106715980+Aiden-Robinson@users.noreply.github.com>
2023-11-26 09:57:20 -0500

    fix: css

--- frontend/src/Game/Tetris/tetris.js
+++ frontend/src/Game/Tetris/tetris.js
@@ -17,7 +17,7 @@ function Tetris() {
   return (
     <div className="tetris">
       <div className='gameContainer'>
-      <iframe title="tetris" src="https://playclassic.games/games/puzzle-solving-nes-games-online/tetris" className="twentyGame" frameborder="0" scrolling="no"></iframe>
+      <iframe title="tetris" src="https://playclassic.games/games/puzzle-solving-nes-games-online/tetris" className="game" frameborder="0" scrolling="no"></iframe>
         <embed className='endpoint' src="http://localhost:5050/tetris"/>
       </div>
       <div className="instructionsContainer">
