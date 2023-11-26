bd0106e5ff18118337acb29a554d6a78a3261711
Aiden Robinson <106715980+Aiden-Robinson@users.noreply.github.com>
2023-11-26 01:58:21 -0500

    added all icons

--- frontend/src/Game/TempleRun/templeRun.js
+++ frontend/src/Game/TempleRun/templeRun.js
@@ -1,5 +1,8 @@
 import './templeRun.css';
-import Left from '../../Assets/Game/Controls/Left.svg';
+import Left from '../../Assets/Game/Controls/Left.png';
+import Right from '../../Assets/Game/Controls/Right.png';
+import Jump from '../../Assets/Game/Controls/Jump.png';
+import Squat from '../../Assets/Game/Controls/Squat.png';
 
 function TempleRun() {
   return (
@@ -9,18 +12,35 @@ function TempleRun() {
         <embed className='endpoint' src="http://localhost:5050/templerun"/>
       </div>
       <div className="instructionsContainer">
-        <div className="armRow">
-          <div className="leftArmContainer">
-              <img className='leftArmIcon' src={Left} alt='Left'/>
+        <div className="row">
+          <div className="iconContainer">
+              <img className='icon' src={Left} alt='Left'/>
               Move left
           </div>
-          <div className="leftArmContainer">
-              <img className='leftArmIcon' src={Left} alt='Left'/>
+          <div className="iconContainer">
+              <img className='icon' src={Right} alt='Right'/>
               Move Right
           </div>
         </div>
-        <div className="jumpRow">
-
+        <div className="row">
+          <div className="iconContainer">
+              <img className='icon' src={Jump} alt='Jump'/>
+              Play Again
+          </div>
+          <div className="iconContainer">
+              <img className='icon' src={Squat} alt='Squat'/>
+              Slide
+          </div>
+        </div>
+        <div className="row">
+          <div className="iconContainer">
+              <img className='icon' src={Jump} alt='Cross arms'/>
+              Jump
+          </div>
+          <div className="iconContainer">
+              <img className='icon' src={Squat} alt='Squat'/>
+              Exit
+          </div>
         </div>
       </div>        
     </div>
