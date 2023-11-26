b9c370063469cc510cc716ee4b359a6a28f5d66c
Aiden Robinson <106715980+Aiden-Robinson@users.noreply.github.com>
2023-11-26 02:13:25 -0500

    Made css names more general

--- frontend/src/Game/TempleRun/templeRun.js
+++ frontend/src/Game/TempleRun/templeRun.js
@@ -9,8 +9,8 @@ import CrossArms from '../../Assets/Game/Controls/CrossArms.png';
 function TempleRun() {
   return (
     <div className="templeRun">
-      <div className='templeRunContainer'>
-        <iframe title="templeRunGame" className="templeRunGame" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" frameborder="0" />
+      <div className='gameContainer'>
+        <iframe title="game" className="game" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" frameborder="0" />
         <embed className='endpoint' src="http://localhost:5050/templerun"/>
       </div>
       <div className="instructionsContainer">
