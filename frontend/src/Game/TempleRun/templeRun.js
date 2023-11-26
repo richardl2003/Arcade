36f36136260d1b80a897a3b2efdcfd16884eb7c4
Luai Bashar <luai.hawa@gmail.com>
2023-11-25 23:54:25 -0500

    removed redundant code from templeRun.js

--- frontend/src/Game/TempleRun/templeRun.js
+++ frontend/src/Game/TempleRun/templeRun.js
@@ -2,16 +2,13 @@ import './templeRun.css';
 
 function TempleRun() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <div className="game">
-          <iframe title="Camera" className="App-game" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" scrolling="none" frameborder="0"></iframe>
-        </div>
-        <div className="App-video">
-          <h3>Video Stream</h3>
-          <embed className='endpoint' src="http://localhost:5050/templerun"/>
-        </div>        
+    <div className="templeRun">
+      <div className='gameContainer'>
+        <iframe title="game" className="game" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" frameborder="0" />
       </div>
+      <div className="App-video">
+        <embed className='endpoint' src="http://localhost:5050/templerun"/>
+      </div>        
     </div>
   );
 }
