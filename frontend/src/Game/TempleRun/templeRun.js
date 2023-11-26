002e455b8702d9c3fd110f65bd80bd54198a49c8
Aiden Robinson <106715980+Aiden-Robinson@users.noreply.github.com>
2023-11-26 01:35:18 -0500

    adding space invaders images

--- frontend/src/Game/TempleRun/templeRun.js
+++ frontend/src/Game/TempleRun/templeRun.js
@@ -1,4 +1,5 @@
 import './templeRun.css';
+import Left from '../../Assets/Game/Controls/Left.svg';
 
 function TempleRun() {
   return (
@@ -7,7 +8,20 @@ function TempleRun() {
         <iframe title="templeRunGame" className="templeRunGame" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" frameborder="0" />
         <embed className='endpoint' src="http://localhost:5050/templerun"/>
       </div>
-      <div className="">
+      <div className="instructionsContainer">
+        <div className="armRow">
+          <div className="leftArmContainer">
+              <img className='leftArmIcon' src={Left} alt='Left'/>
+              Move left
+          </div>
+          <div className="leftArmContainer">
+              <img className='leftArmIcon' src={Left} alt='Left'/>
+              Move Right
+          </div>
+        </div>
+        <div className="jumpRow">
+
+        </div>
       </div>        
     </div>
   );
