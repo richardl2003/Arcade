a12d34d80328b289b69e43d8ec19014cf91562f9
Luai Bashar <luai.hawa@gmail.com>
2023-11-25 21:12:24 -0500

    removed useless camera code

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from camera import VideoCamera
 import subprocess
 
 app = Flask(__name__)
--- frontend/src/Game/Mario/mario.js
+++ frontend/src/Game/Mario/mario.js
@@ -1,21 +1,6 @@
 import './mario.css';
-import { useEffect, useState } from 'react';
-import Webcam from "react-webcam";
-import axios from 'axios';
 
 function Mario() {
-  const [videoSrc, setVideoSrc] = useState('');
-  useEffect(() => {
-
-    const fetchData = async () => {
-      const response = await axios.get('http://localhost:5050/');
-      setVideoSrc(URL.createObjectURL(new Blob([response.data])));
-    }; 
-    
-    fetchData();
-  }, [])
-
-
   return (
     <div className="App">
       <div className="App-header">
@@ -23,7 +8,6 @@ function Mario() {
           <iframe title="Camera" className="App-game" src="https://playclassic.games/games/jump-and-run-snes-games-online/play-super-mario-world-online/play/" scrolling="none" frameborder="0"></iframe>
         </div>
         <div className="App-video">
-          <h3>Video Stream</h3>
           <img className="App-video-feed" src="http://localhost:5050/mario" alt="video stream" />
         </div>        
       </div>
