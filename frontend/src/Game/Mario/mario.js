db8ba70af113b3996b89b76f4721f2eeecbedd49
Luai Bashar <luai.hawa@gmail.com>
2023-11-26 00:22:21 -0500

    Made css names more descriptive

--- frontend/src/Game/Mario/mario.js
+++ frontend/src/Game/Mario/mario.js
@@ -2,16 +2,13 @@ import './mario.css';
 
 function Mario() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <div className="game">
-          <iframe title="Camera" className="App-game" src="https://playclassic.games/games/jump-and-run-snes-games-online/play-super-mario-world-online/play/" scrolling="none" frameborder="0"></iframe>
-        </div>
-        <div className="App-video">
-          <h3>Video Stream</h3>
-          <embed className='endpoint' src="http://localhost:5050/mario"/>
-        </div>        
+    <div className="mario">
+      <div className='marioGameContainer'>
+      <iframe title="marioGame" className="marioGame" src="https://playclassic.games/games/jump-and-run-snes-games-online/play-super-mario-world-online/play/" scrolling="none" frameborder="0"/>
+        <embed className='endpoint' src="http://localhost:5050/mario"/>
       </div>
+      <div className="">
+      </div>        
     </div>
   );
 }
--- frontend/src/Game/TempleRun/templeRun.js
+++ frontend/src/Game/TempleRun/templeRun.js
@@ -3,11 +3,11 @@ import './templeRun.css';
 function TempleRun() {
   return (
     <div className="templeRun">
-      <div className='gameContainer'>
-        <iframe title="game" className="game" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" frameborder="0" />
-      </div>
-      <div className="App-video">
+      <div className='templeRunContainer'>
+        <iframe title="templeRunGame" className="templeRunGame" src="https://html5.gamedistribution.com/f2af2ecc05a445edb6862c589e996a7e/" frameborder="0" />
         <embed className='endpoint' src="http://localhost:5050/templerun"/>
+      </div>
+      <div className="">
       </div>        
     </div>
   );
